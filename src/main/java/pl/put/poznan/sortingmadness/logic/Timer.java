8cbb2c24344d93b90036c865774b387a0c51c1b7
rychu-tech <rychudotka14@gmail.com>
2021-12-19 19:32:07 +0100

    [#13] Add Timer

---
+++ src/main/java/pl/put/poznan/sortingmadness/logic/Timer.java
@@ -0,0 +1,20 @@
+package pl.put.poznan.sortingmadness.logic;
+
+public class Timer {
+    private long start;
+    private long end;
+    
+    public void startTimer() {
+        this.start = System.currentTimeMillis();
+    }
+
+    public void stopTimer() {
+        this.end = System.currentTimeMillis();
+    }
+
+    public long getTime() {
+        long time = this.end - this.start;
+        return time;
+    }
+
+}
