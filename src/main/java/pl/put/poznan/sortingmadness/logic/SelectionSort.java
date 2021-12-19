4530f18c448548133734ef51b612038b093cf201
czarnobylu <jbylicki@hotmail.com>
2021-12-19 18:27:34 +0100

    [#09] SelectionSort: Add sortSimple method
    Add the sortSimple method for SelectionSort algorithm
    
    Signed-off-by: czarnobylu <jbylicki@hotmail.com>

---
+++ src/main/java/pl/put/poznan/sortingmadness/logic/SelectionSort.java
@@ -0,0 +1,35 @@
+package pl.put.poznan.sortingmadness.logic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SelectionSort implements SortingStrategy {
+    private static void ss(String arr[], int noIter){
+        int len = arr.length;
+        for (int i = 0; i < len-1 && (i < noIter || noIter == -1); i++)
+        {
+            int min = i;
+            for (int j = i+1; j < arr.length; j++)
+                if (arr[j].compareTo(arr[min]) < 0)
+                    min = j;
+            String temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    @Override
+    public ArrayList<String> sortSimple(ArrayList<String> list, int noIter)
+    {
+        String[] arr = list.toArray(new String[0]);
+        ss(arr, noIter);
+        list = new ArrayList<String>();
+        Collections.addAll(list, arr);
+        return list;
+
+    }
+    @Override
+    public ArrayList<ArrayList<String>> sortNested(ArrayList<ArrayList<String>> list, int noIter, String column)
+    {
+        return new ArrayList<ArrayList<String>>();
+    }
+}
